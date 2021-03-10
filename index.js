0ff28c7a168b26eae2fa06ed68cccd5d005e1ddd
Michal <michal.p.zarzycki@wp.pl>
2021-03-10 02:12:40 +0100

    Added OAuth2

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
 const dotenv = require('dotenv')
-
+const nodemailer = require('nodemailer')
+const {google} = require('googleapis')
 dotenv.config()
+
+const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI)
+
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN})
+
+async function sendMail() {
+    try{
+        const accessToken = await oAuth2Client.getAccessToken()
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user:'mzarzycki37@gmail.com',
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                refreshToken: process.env.REFRESH_TOKEN,
+                accessToken: accessToken
+            },
+        })
+        let mailOption = {
+            from:  'mzarzycki37@gmail.com',
+            to: 'michal.p.zarzycki@wp.pl',
+            subject: "HI MIKE!",
+            text: "How u doing?"
+        }
+     const result = await transporter.sendMail(mailOption, (err, data) => {
+            if(err) {
+                console.log('ERROR 📽', err)
+            } else {
+                console.log("MAIL SENDED 🔥" )
+            }
+        })
+        return result
+    } catch(err) {
+        return err
+    }
+};
+
+for(let i=0; i<10; i++) {
+    sendMail()
+}
+
+
+
+// 753789895976-ue2tq2pspa1qjdbf4mj237tnhor2r8au.apps.googleusercontent.com
+// POJ9BVBqAIDV6xSwGt4JH1_t
